2e334ef73808a0881992b64cda68f97c0151657f
SrijanG05 <gsrijan777@gmail.com>
2026-04-10 14:56:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function indexOfIgnoreCase(s1, s2) {
   // write your code here
 	s1=s1.toLowercase();
 	s2=s2.toLowercase();
-	let substr;
+	let result;
 	for(let i=0;i<s1.length;i++)
 		{
          if(s1[i]===s2.charAt[0])
 		 {
-          substr=s1.substr(i,s1.length-1);
+          result=s1.substr(i,s1.length-1);
 		 }
 		}
 			else
 		 {
           return -1;
 		 }
-	console.log(substr);
+	return result;
 		}
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
