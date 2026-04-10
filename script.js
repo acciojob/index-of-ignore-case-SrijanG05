eb1b45deb9b6efed2f3e1115356ff4a585192c9f
SrijanG05 <gsrijan777@gmail.com>
2026-04-10 14:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function indexOfIgnoreCase(s1, s2) {
 		 }
 	console.log(substr);
 		}
-        
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));        
 
 // Please do not change the code below
